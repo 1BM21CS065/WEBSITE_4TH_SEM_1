61311cf20bb2d6e574d2f194ba6cb0c11fe9363a
1BM21CS065 <117078173+1BM21CS065@users.noreply.github.com>
2023-09-05 15:54:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/homepage/webpage.html/');
+	header('Location: '.$uri.'/homepage/webpage.html');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
